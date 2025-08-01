55cc94b2d810604a43e2d58daef9e00d5b603f15
Rodrigo Ramos <rodrigoramosgja@gmail.com>
2025-08-01 13:57:12 +0000

    Cole o código abaixo na tag <head> do seu site. Ao fazer isso, o VTurb v

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,14 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
+        <link rel="preload" href="https://scripts.converteai.net/d5647b15-021e-4678-89a8-e5966099f778/players/688cc51742446e639fafab62/player.js" as="script" />
+        <link rel="preload" href="https://scripts.converteai.net/lib/js/smartplayer/v1/smartplayer.min.js" as="script" />
+        <link rel="preload" href="https://images.converteai.net/d5647b15-021e-4678-89a8-e5966099f778/players/688cc51742446e639fafab62/thumbnail.jpg" as="image" />
+        <link rel="preload" href="https://cdn.converteai.net/d5647b15-021e-4678-89a8-e5966099f778/688cc51251ec00e4ce1df9e6/main.m3u8" as="fetch" />
+        <link rel="dns-prefetch" href="https://cdn.converteai.net" />
+        <link rel="dns-prefetch" href="https://scripts.converteai.net" />
+        <link rel="dns-prefetch" href="https://images.converteai.net" />
+        <link rel="dns-prefetch" href="https://api.vturb.com.br" />
       </head>
       <body className="font-body antialiased">
         {children}
